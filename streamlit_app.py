33e19671a1c78b8fc32461ce24e74f76fdb054e2
piyush pathak <117860797+piyushsnowflake@users.noreply.github.com>
2022-11-12 10:35:30 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -40,18 +40,27 @@ except URLError as e:
 
 
 
-streamlit.header("The fruit load list contains:")
+#streamlit.header("The fruit load list contains:")
+streamlit.header("View our Fruit List - Add Your Favorites!")
 def get_fruit_load_list():
     with my_cnx.cursor() as my_cur:
          my_cur.execute("SELECT * from fruit_load_list")
          return my_cur.fetchall()
 
-if streamlit.button('Get Fruit Load List'):
+if streamlit.button('Get Fruit List'):
    my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
    my_data_rows = get_fruit_load_list()
+    my_cnx.close()
    streamlit.dataframe(my_data_rows)
-                    
+
+def insert_row_snowflake(new_fruit):
+    with mu_cnx.cursor() as my_cur:
+        my_cur.execute("insert into fruit_load_list values ('" + new_fruit + "') ")
+        return "Thanks for adding " + new fruit
+    
 streamlit.stop()
-add_my_fruit = streamlit.text_input('What fruit would you like to add?','jackfruit')
-streamlit.write('Thanks for adding ', add_my_fruit)
-my_cur.execute("insert into fruit_load_list values ('from streamlite') ")
+add_my_fruit = streamlit.text_input('What fruit would you like to add?')
+if add_my_fruit:
+    streamlit.error("Please select a fruit to get information.")
+    streamlit.write(insert_row_snowflake(add_my_fruit)
+
